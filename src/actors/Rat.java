7bc28976b42aed3fae27c58e052dad1c89308554
Ryan <pennamancer@gmail.com>
2021-11-27 00:56:14 -0400

    Add a Special Attack to Goblin and Rat Actors
    - Goblin and Rat now have a chance to perform a more powerful attack.
    - 1 in 5 for Goblin, 1 in 100 for Rat

--- src/actors/Rat.java
+++ src/actors/Rat.java
@@ -2,6 +2,8 @@
 
 package src.actors;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Rat extends Actor{
     private static final String NAME = "Rat";
     private static final int HEALTH = 6;
@@ -14,5 +16,33 @@ public class Rat extends Actor{
     // Constructor
     public Rat() {
         super(NAME, HEALTH, ARMOR, MANA, BASEDAMAGE, EXPVALUE, STARTINGLOCATION);
+    }
+
+    // General Methods
+    public void attack(Actor targetActor) {
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, 101); // Random number from 1 - 100
+        if (randomNumber == 1) {
+            gnaw(targetActor);
+        } else {
+            bite(targetActor);
+        }
+    }
+
+    // Extremely powerful comedy attack
+    private void gnaw(Actor targetActor) {
+        if (super.getMana() >= 1200) {
+            super.setMana(super.getMana() - 1200);
+            System.out.println("The Rat channels an eternity of subjugation and torment. The world goes black. " + targetActor.getName() + 
+                                " takes " + (targetActor.getHealth() - 1) + " damage. They are holding on by a thread.");
+            targetActor.setHealth(1);
+
+        } else {
+            System.out.println("The Rat tries to channel its anger once more. Nothing happens.");
+        }
+    }
+
+    // Regular attack
+    private void bite(Actor targetActor) {
+        super.attack(targetActor);
     }
 }
